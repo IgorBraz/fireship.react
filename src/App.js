8548a3c563d259cb4a05703406410d72ada3e072
IgorBraz <igorbraz81@outlook.com>
2023-01-07 04:11:32 -0300

    Anatomy | Components | Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent'
+import React, { useState } from 'react';
+import './style.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Ok I just edited it!
         </p>
+        <MyComponent name={'Igor'} />
+        <Card icon={<Icon></Icon>}>
+          <p>The body of the card</p>
+        </Card>
+        <LoadingButton
+          label="Press me"
+          loading={isLoading}
+          onClick={() => setIsLoading(!isLoading)}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,3 +38,20 @@ function App() {
 }
 
 export default App;
+
+function Card(props) {
+  return <section>
+    <h2>{props.icon} Title</h2>
+    {props.children}
+  </section>;
+}
+
+function Icon() {
+  return <i>🔥</i>;
+}
+
+function LoadingButton({ onClick, loading, label }) {
+  return <button onClick={onClick} type="button">
+    {loading ? <div className='loader' /> : label}
+  </button>
+}
---
+++ src/MyComponent.jsx
@@ -0,0 +1,5 @@
+function MyComponent({ name }) {
+    return <p>🔥 Hello {name}!</p>
+}
+
+export default MyComponent;
